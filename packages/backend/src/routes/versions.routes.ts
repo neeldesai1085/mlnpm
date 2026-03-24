9020ec0c656f0db59a676fc3cc578c32f4da2e1f
Neel Desai <neeldesai1085@gmail.com>
2026-03-25 00:57:01 +0530

    Inserted mergeParams to version router to allow get parameter in version controller

--- packages/backend/src/routes/versions.routes.ts
+++ packages/backend/src/routes/versions.routes.ts
@@ -2,7 +2,7 @@ import { Router } from "express";
 import { publishVersion, getVersion } from "../controllers/versions.controller.js";
 import { requireAuth } from "../middleware/auth.middleware.js";
 
-const router = Router();
+const router = Router({ mergeParams: true });
 
 router.post("/", requireAuth, publishVersion);
 router.get("/:version", getVersion);
