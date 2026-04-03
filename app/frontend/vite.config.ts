26cda8ec52ce82f515b73ff498ce5cf9de55a576
Neel Desai <neeldesai1085@gmail.com>
2026-04-04 02:01:26 +0530

    Changed the api target again as it is not read during the production system

--- app/frontend/vite.config.ts
+++ app/frontend/vite.config.ts
@@ -9,7 +9,7 @@ export default defineConfig({
         port: 5173,
         proxy: {
             "/api": {
-                target: "https://mlnpm.onrender.com",
+                target: "http://localhost:3000",
                 changeOrigin: true,
                 rewrite: (path) => path.replace(/^\/api/, ""),
             },
