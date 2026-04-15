db714ab4548f915459842c69b3aadb8714e0d0d4
Neel Desai <neeldesai1085@gmail.com>
2026-04-15 16:52:27 +0530

    Tested all the commands and created index file to make all the commands accessible

---
+++ app/cli/src/index.ts
@@ -0,0 +1,66 @@
+#!/usr/bin/env node
+
+import { Command } from "commander";
+import { installCommand } from "./commands/install.js";
+import { uninstallCommand } from "./commands/uninstall.js";
+import { restoreCommand } from "./commands/restore.js";
+import { listCommand } from "./commands/list.js";
+import { cacheListCommand, cacheCleanCommand } from "./commands/cache.js";
+
+const program = new Command();
+
+program
+    .name("mlnpm")
+    .description("The Distributed Machine Learning Package Manager")
+    .version("1.0.0");
+
+program
+    .command("install <name>")
+    .alias("i")
+    .description("Install an ML model package into your project")
+    .action(async (name: string) => {
+        await installCommand(name);
+    });
+
+program
+    .command("uninstall <name>")
+    .alias("rm")
+    .description("Remove an mlnpm package from your project")
+    .action(async (name: string) => {
+        await uninstallCommand(name);
+    });
+
+program
+    .command("restore")
+    .description("Restore all mlnpm packages from package.json (runs in postinstall)")
+    .action(async () => {
+        await restoreCommand();
+    });
+
+program
+    .command("list")
+    .alias("ls")
+    .description("List all mlnpm packages in this project")
+    .action(async () => {
+        await listCommand();
+    });
+
+const cache = program
+    .command("cache")
+    .description("Manage the global model cache (~/.mlnpm/cache/)");
+
+cache
+    .command("list")
+    .description("Show all cached models and their sizes")
+    .action(async () => {
+        await cacheListCommand();
+    });
+
+cache
+    .command("clean [name]")
+    .description("Clear the cache (optionally for a specific package)")
+    .action(async (name?: string) => {
+        await cacheCleanCommand(name);
+    });
+
+program.parse(process.argv);
